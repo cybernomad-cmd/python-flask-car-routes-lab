da13cc3891c0c5d25e11488cb25c2dc636f4fa30
Leon Muigai <leonmuigaike@gmail.com>
2026-06-25 12:58:40 +0300

    Improve welcome message

--- server/app.py
+++ server/app.py
@@ -1,12 +1,27 @@
 from flask import Flask
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 existing_models = [
-  "Toyota Probox"
-  "Harrier"
-  "Range Rover"
-  "Volkswagen"
+    "Nissan",
+    "Volkswagen",
+    "Range Rover",
+    "Lexus"
 ]
 
+
 @app.route("/")
+def home():
+    return "Welcome to the Car Company. Browse our available models."
+
+
+@app.route("/models/<string:model>")
+def get_model(model):
+    if model in existing_models:
+        return f"{model} is available."
+
+    return f"{model} is not available.", 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
